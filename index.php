9e64d0993b14217fda12d9f2f7a6773539552346
samir bostani <s.bostani2@ymail.com>
2018-02-21 02:23:59 +0330

    add jquery and bootstrap file

--- index.php
+++ index.php
@@ -1,9 +1,15 @@
 <html>
 <head>
+  <script  src="css/bootstrap-grid.css"  type="text/javascript" </script>
+  <script  src="css/bootstrap.css"  type="text/javascript" </script>
+  <script  src="js/bootstrap.js"  type="text/javascript" </script>
+  <script  src="js/jquery-3.3.1.js"  type="text/javascript" </script>
+
 <title>ToDo List</title>
 </head>
 
 <body>
-  <?php echo "hello world"; ?>
+  <?php echo "hello samir"; ?>
+
 </body>
 </html>
