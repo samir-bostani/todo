066297e3f14a0ba81bc7bbd4fc4247bc3e0c1a68
samir bostani <s.bostani2@ymail.com>
2018-02-21 02:53:13 +0330

    config file

--- index.php
+++ index.php
@@ -1,15 +1,19 @@
 <html>
 <head>
-  <script  src="css/bootstrap-grid.css"  type="text/javascript" </script>
-  <script  src="css/bootstrap.css"  type="text/javascript" </script>
-  <script  src="js/bootstrap.js"  type="text/javascript" </script>
-  <script  src="js/jquery-3.3.1.js"  type="text/javascript" </script>
+  <link rel="stylesheet" href="css/bootstrap.css" </link>
+  
 
 <title>ToDo List</title>
 </head>
 
 <body>
   <?php echo "hello samir"; ?>
+  <h1> bootstrap </h1>
+  <button class="btn btn-success">click Me </button>
+
+
+  <script  src="js/jquery-3.3.1.js" />
+  <script  src="js/bootstrap.js" />
 
 </body>
 </html>
