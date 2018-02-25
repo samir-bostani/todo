d9347c379eeb93f6aa498b937d8cdf46b0a8dab2
samir bostani <s.bostani2@ymail.com>
2018-02-25 23:44:36 +0330

    ok

--- index.php
+++ index.php
@@ -4,12 +4,17 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <title></title>
+    <title>todo program</title>
 
     <link rel="stylesheet" href="css/bootstrap.min.css" >
   </head>
   <body>
-    <h1>Hello, world!</h1>
+    <div class="jumbotron">
+      <h1>Hello, world!</h1>
+      <img height="500px" width="500px" src="img/img_01.jpg" alt="" class="rounded-circle">
+      <img height="500px" width="500px" src="img/img_04.jpg" alt="" class="rounded-circle">
+    </div>
+
 
     <script src="js/jquery-3.3.1.js"></script>
     <script src="js/bootstrap.min.js"></script>
