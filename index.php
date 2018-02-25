e71e71e14bf2f78439e76eef99a26656ff49a6dd
samir bostani <s.bostani2@ymail.com>
2018-02-25 09:51:20 -0500

    test

--- index.php
+++ index.php
@@ -1,19 +1,40 @@
-<html>
-<head>
-  <link rel="stylesheet" href="css/bootstrap.css" </link>
-  
+  <!DOCTYPE html>
+  <html>
+    <head>
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <meta http-equiv="x-ua-compatible" content="ie=edge">
+      <link rel="stylesheet" href="css/bootstrap.css" >
+      <title>ToDo</title>
+    </head>
+    <body>
+      <br/>
+      <br/>
 
-<title>ToDo List</title>
-</head>
+      <p>Envelope icon: <span class="glyphicon glyphicon-envelope"></span></p>
+     <p>Envelope icon as a link:
+       <a href="#"><span class="glyphicon glyphicon-envelope"></span></a>
+     </p>
+     <p>Search icon: <span class="glyphicon glyphicon-search"></span></p>
+     <p>Search icon on a button:
+       <button type="button" class="btn btn-default">
+         <span class="glyphicon glyphicon-search"></span> Search
+       </button>
+     </p>
+     <p>Search icon on a styled button:
+       <button type="button" class="btn btn-info">
+         <span class="glyphicon glyphicon-search"></span> Search
+       </button>
+     </p>
+     <p>Print icon: <span class="glyphicon glyphicon-print"></span></p>
+     <p>Print icon on a styled link button:
+       <a href="#" class="btn btn-success btn-lg">
+         <span class="glyphicon glyphicon-print"></span> Print
+       </a>
+     </p> 
 
-<body>
-  <?php echo "hello samir"; ?>
-  <h1> bootstrap </h1>
-  <button class="btn btn-success">click Me </button>
 
-
-  <script  src="js/jquery-3.3.1.js" />
-  <script  src="js/bootstrap.js" />
-
-</body>
-</html>
+      <script src="js/jquery-3.3.1.js"></script>
+      <script src="js/bootstrap.min.js" ></script>
+    </body>
+  </html>
